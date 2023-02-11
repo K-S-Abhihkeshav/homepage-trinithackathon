927ae24265a1a8d19b3f1cc68d3f697d2c60d8e1
K-S-Abhihkeshav <97213738+K-S-Abhihkeshav@users.noreply.github.com>
2023-02-11 14:11:27 +0530

    form.js added, completed basic starting page, made changes in style
    form and about section created
    no linking to node yet
    removed scrollbar

--- homepage.js
+++ homepage.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "/src/styles.css";
-
+import create from "/src/form.js";
 function home(){
     return(
         <body>
-            <nav align="right">
-                <a href=""><button>login</button></a>
-                <a href=""><button>signup</button></a>
-            </nav>
-            <h1 classname="title">title</h1>            
-            <div>
+          <h1 align="center">AGRI-CONNECT</h1>
+            <ul><h2>ABOUT US -></h2>
+                <li>A social media platforms to help farmers and farming enthusiasts.</li>
+                <li>We connect farmers from all over India with the latest advancements in agricultural technology.</li>
+                <li>we suggest crops for farmers to grow based on their
+                specific field and family information.</li>
+                <li>Provide information on available schemes and programs from the central and state governments.</li>
+                <li> We also provide various hybrid farming methods and technology</li>
+            </ul>
                 <p>
-                    <h2>DESCRIPTION</h2>
-                    some paragraph
+                {create()}
                 </p>
-            </div>
         </body>
     );
 }
